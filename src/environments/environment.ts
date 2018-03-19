eb3a0347608652aa4cd45e3c9bc7b029f19d078a
Bezael Pérez <bezael_e@hotmail.com>
2018-03-19 20:00:30 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCnrm93axlwbHzXEVA8smrQVw-i3OJMoEY",
-    authDomain: "loginapp-c7ad7.firebaseapp.com",
-    databaseURL: "https://loginapp-c7ad7.firebaseio.com",
-    projectId: "loginapp-c7ad7",
-    storageBucket: "loginapp-c7ad7.appspot.com",
-    messagingSenderId: "185540221956"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "l",
+    messagingSenderId: ""
   }
 };
